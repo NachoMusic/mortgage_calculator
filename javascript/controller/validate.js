ff538d6fb0fbd479e4c3c4d13a01b63f4dbc3631
Nacho Valera Herrera <nachovaleraherreraes@gmail.com>
2016-10-18 04:00:49 +0200

    Validation improved and a bug in the total interest fixed

--- javascript/controller/validate.js
+++ javascript/controller/validate.js
@@ -9,10 +9,18 @@ function validate_num(num) {
     }
     return valid;
 }
+function validate_age(num) {
+    var valid = false;
+    if (num.length > 0) {
+        var regexp = /^(0?[1-9]|[1-9][0-9]|[1][1-9][1-9]|200)$/;
+        valid = regexp.test(num);
+    }
+    return valid;
+}
 function validate_nif(nif) {
     var valid = false;
-    if (dni.length > 0) {
-        var regexp = /^([0-9]{8})[A-Za-z]{1}$/i;
+    if (nif.length > 0) {
+        var regexp = /^([0-9]{8})[A-Za-z]{1}$/;
         valid = regexp.test(nif);
     }
     return valid;
@@ -22,6 +30,22 @@ function validate_phone(phone) {
     if (phone.length > 0) {
         var regexp = /^((\+?34([ \t|\-])?)?[9|6|7]((\d{1}([ \t|\-])?[0-9]{3})|(\d{2}([ \t|\-])?[0-9]{2}))([ \t|\-])?[0-9]{2}([ \t|\-])?[0-9]{2})$/;
         valid = regexp.test(phone);
+    }
+    return valid;
+}
+function validate_email(email) {
+    var valid = false;
+    if (email.length > 0) {
+        var regexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
+        valid = regexp.test(email);
+    }
+    return valid;
+}
+function validate_text(text) {
+    var valid = false;
+    if (text.length > 0) {
+        var regexp = /^[A-Za-z ]{2,20}$/;
+        valid = regexp.test(text);
     }
     return valid;
 }
@@ -59,18 +83,7 @@ function Validate() {
         } else {
             document.getElementById('e_capital').innerHTML = "";
         }
-        if (!validate_num(euribor+"")) {
-            valid = false;
-            document.getElementById('e_euribor').innerHTML = "Should be a number";
-        } else {
-            document.getElementById('e_euribor').innerHTML = "";
-        }
-        if (!validate_num(differential+"")) {
-            valid = false;
-            document.getElementById('e_differential').innerHTML = "Should be a number";
-        } else {
-            document.getElementById('e_differential').innerHTML = "";
-        }
+
         if (!validate_num(period)){
             valid = false;
             document.getElementById('e_period').innerHTML = "Should be a number";
@@ -98,11 +111,32 @@ function Validate() {
             euribor_type.readOnly = false;
             differential_type.readOnly = false;
             interestApplied.value = euribor + differential - discount;
+            if (!validate_num(euribor+"")) {
+                valid = false;
+                document.getElementById('e_euribor').innerHTML = "Should be a number";
+            } else {
+                document.getElementById('e_euribor').innerHTML = "";
+            }
+            if (!validate_num(differential+"")) {
+                valid = false;
+                document.getElementById('e_differential').innerHTML = "Should be a number";
+            } else {
+                document.getElementById('e_differential').innerHTML = "";
+            }
+            document.getElementById('e_fixed_interest').innerHTML = "";
         } else {
             euribor_type.readOnly = true;
             differential_type.readOnly = true;
             fixed_interest_type.readOnly = false;
             interestApplied.value = fixed_interest - discount;
+            document.getElementById('e_euribor').innerHTML = "";
+            document.getElementById('e_differential').innerHTML = "";
+            if (!validate_num(fixed_interest)) {
+                valid = false;
+                document.getElementById('e_fixed_interest').innerHTML = "Should be a number";
+            } else {
+                document.getElementById('e_fixed_interest').innerHTML = "";
+            }
         }
         console.log(valid);
         if (valid) {
@@ -112,7 +146,7 @@ function Validate() {
                         period * 12)));
             monthly_quote.value = total;
 
-            var interestTotal = (total*12)-capital;
+            var interestTotal = (total* (period * 12) )-capital;
             document.getElementById('interestTotal').innerHTML = "At the end you pay " + interestTotal + " interest";
         }
 
@@ -124,6 +158,49 @@ function Validate() {
         var age = document.getElementById('age').value;
         var mobile = document.getElementById('mobile').value;
         var email = document.getElementById('email').value;
+
+        if (!validate_nif(nif)) {
+            valid = false;
+            document.getElementById('e_nif').innerHTML = "Write a correct nif";
+        } else {
+            document.getElementById('e_nif').innerHTML = "";
+        }
+        if (!validate_text(nombre)) {
+            valid = false;
+            document.getElementById('e_nombre').innerHTML = "Write a text name > 1 char";
+        } else {
+            document.getElementById('e_nombre').innerHTML = "";
+        }
+        if (!validate_text(surname1)) {
+            valid = false;
+            document.getElementById('e_surname1').innerHTML = "Write a text surname > 1 char";
+        } else {
+            document.getElementById('e_surname1').innerHTML = "";
+        }
+        if (!validate_text(surname2)) {
+            valid = false;
+            document.getElementById('e_surname2').innerHTML = "Write a text surname > 1 char";
+        } else {
+            document.getElementById('e_surname2').innerHTML = "";
+        }
+        if (!validate_age(age)) {
+            valid = false;
+            document.getElementById('e_age').innerHTML = "Write a correct age";
+        } else {
+            document.getElementById('e_age').innerHTML = "";
+        }
+        if (!validate_phone(mobile)) {
+            valid = false;
+            document.getElementById('e_mobile').innerHTML = "Write a correct mobile number";
+        } else {
+            document.getElementById('e_mobile').innerHTML = "";
+        }
+        if (!validate_email(email)) {
+            valid = false;
+            document.getElementById('e_email').innerHTML = "Write a correct email";
+        } else {
+            document.getElementById('e_email').innerHTML = "";
+        }
 
         if(valid){
             submitbutton.disabled=false;
